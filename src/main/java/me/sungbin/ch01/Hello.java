c881cb3ed04139ff1a3b21f91eab27d1554f15dc
SungbinYang <ysb05222@naver.com>
2022-05-10 22:00:51 +0900

    자바를 시작하기 전에

---
+++ src/main/java/me/sungbin/ch01/Hello.java
@@ -0,0 +1,19 @@
+package me.sungbin.ch01;
+
+/**
+ * packageName : me.sungbin.ch01
+ * fileName : Hello
+ * author : rovert
+ * date : 2022/05/10
+ * description :
+ * ===========================================================
+ * DATE 			AUTHOR			 NOTE
+ * -----------------------------------------------------------
+ * 2022/05/10       rovert         최초 생성
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
