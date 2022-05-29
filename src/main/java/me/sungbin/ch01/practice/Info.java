6244b63c3c79d68c6e07b166168971b65f32efdb
SungbinYang <ysb05222@naver.com>
2022-05-29 12:59:56 +0900

    ch01 실전예제

---
+++ src/main/java/me/sungbin/ch01/practice/Info.java
@@ -0,0 +1,20 @@
+package me.sungbin.ch01.practice;
+
+/**
+ * @author : rovert
+ * @packageName : me.sungbin.ch01.practice
+ * @fileName : Info
+ * @date : 2022/05/29
+ * @description :
+ * ===========================================================
+ * DATE 			AUTHOR			 NOTE
+ * -----------------------------------------------------------
+ * 2022/05/29       rovert         최초 생성
+ */
+
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("저의 이름은 양성빈입니다.");
+        System.out.println("제가 사는 곳은 경기도입니다.");
+    }
+}
