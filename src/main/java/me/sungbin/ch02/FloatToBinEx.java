813448d7b7d0e260552fe3c79b7f4e42ad88efdb
SungbinYang <ysb05222@naver.com>
2022-06-01 15:03:58 +0900

    기본형

--- src/main/java/me/sungbin/ch02/FloatToBinEx.java
+++ src/main/java/me/sungbin/ch02/FloatToBinEx.java
@@ -1,18 +1,18 @@
 package me.sungbin.ch02;
 
 /**
- * packageName : me.sungbin.ch02
- * fileName : FloatEx2
- * author : rovert
- * date : 2022/05/17
- * description :
+ * @author : rovert
+ * @packageName : me.sungbin.ch02
+ * @fileName : FlaotToBinEx
+ * @date : 2022/06/01
+ * @description :
  * ===========================================================
  * DATE 			AUTHOR			 NOTE
  * -----------------------------------------------------------
- * 2022/05/17       rovert         최초 생성
+ * 2022/06/01       rovert         최초 생성
  */
 
-public class FloatEx2 {
+public class FloatToBinEx {
     public static void main(String[] args) {
         float f = 9.1234567f;
         int i = Float.floatToIntBits(f);
